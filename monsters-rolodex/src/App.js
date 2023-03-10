ffc90df70c7624b274b32a33f2ae2018dc615a68
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-10 11:06:51 -0500

    Add functionality to switch display 1. change combine first and last name variable into a "name" object containing first and last name 2. add "buttonText" to this.state so it can be manipulated 3. create a "handleClick "function to handle toggling states using the button 4. add a "prevState" function to hold a copy of the previous state to be toggled    1. The extra space at the end of the line is added as a separate string to  ensure there is a space between    "lastName" and the next word in the  sentence. If sentences are bunched up in the future     ***you will remember to add this feature to the code.***

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,38 +1,51 @@
-import { Component } from 'react';
-
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      lastName: "Wilson",
-      firstName: "Ryan",
+      name: { firstName: "Ryan", lastName: "Wilson" },
       content: "React Professional",
+      buttonText: "Keep it Real!",
       isPro: true,
     };
-
   }
+
+  handleClick = () => {
+    if (!this.prevState) {
+      this.prevState = { ...this.state };
+    }
+    this.setState(
+      (prevState) => ({
+        name: prevState.isPro
+          ? { firstName: "The", lastName: "Dred" }
+          : { firstName: "Ryan", lastName: "Wilson" },
+        content: prevState.isPro ? "React Code Warlock!" : "React Professional",
+        buttonText: prevState.isPro ? "Back to Pro!" : "ProMode",
+        isPro: !prevState.isPro,
+      }),
+      () => {
+        if (this.state.buttonText === "ProMode") {
+          this.setState(this.prevState);
+          delete this.prevState;
+        }
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            My name is {this.state.firstName} {this.state.lastName}
+            My name is {this.state.name.firstName} {this.state.name.lastName}{" "}
+            {/* Fixed: access first and last name from name object */}
           </p>
-          <button
-            onClick={() => {
-              this.setState({
-                firstName: "The",
-                lastName: "Dread",
-                content: "Code Warlock",
-              });
-            }}
-          >
-            Keep it Real!
-          </button>
+          <button onClick={this.handleClick}>{this.state.buttonText}</button>
+
           <p>{this.state.content}</p>
         </header>
       </div>
@@ -41,5 +54,3 @@ class App extends Component {
 }
 
 export default App;
-
-
