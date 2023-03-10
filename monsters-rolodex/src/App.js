5ff9226890b8cf7c907097b628594b21e0cd3799
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-10 18:43:24 -0500

    Reset to before fun stuff

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,53 +1,31 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Ryan", lastName: "Wilson" },
+      firstName: "Ryan",
+      lastName: "Wilson",
+      firstName: "Ryan",
+      lastName: "Wilson",
       content: "React Professional",
-      buttonText: "Keep it Real!",
       isPro: true,
     };
+
   }
-
-  handleClick = () => {
-    if (!this.prevState) {
-      this.prevState = { ...this.state };
-    }
-    this.setState(
-      (prevState) => ({
-        name: prevState.isPro
-          ? { firstName: "The", lastName: "Dred" }
-          : { firstName: "Ryan", lastName: "Wilson" },
-        content: prevState.isPro ? "React Code Warlock!" : "React Professional",
-        buttonText: prevState.isPro ? "Back to Pro!" : "ProMode",
-        isPro: !prevState.isPro,
-
-
-      }),
-      () => {
-        if (this.state.buttonText === "ProMode") {
-          this.setState(this.prevState);
-          delete this.prevState;
-        }
-      }
-    );
-  };
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            My name is {this.state.name.firstName} {this.state.name.lastName}{" "}
+            My name is {this.state.firstName} {this.state.lastName}
           </p>
-          <button onClick={this.handleClick}>{this.state.buttonText}</button>
-
-          <p>{this.state.content}</p>
+          <button onClick={() =>{}}>Change Name</button>
+           <p>React Wizard</p>
         </header>
       </div>
     );
@@ -55,3 +33,5 @@ class App extends Component {
 }
 
 export default App;
+
+
