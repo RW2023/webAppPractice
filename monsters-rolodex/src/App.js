108ba35ec9be427e02b4a9dc5e54c736e74fe770
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-10 01:46:34 -0500

    Create constructor function: This is a constructor function for a React component that sets the initial state of the component. In React, the constructor() function is used to initialize the component's state and bind event handlers to the component. Here, the constructor function is calling the super() method, which calls the constructor of the parent class (in this case, the Component class from the React library). This is necessary to ensure that the component's state is properly initialized. The constructor is then defining the initial state of the component by setting a property called name to the string value of 'Ryan'. The state object is a special property in React that holds the component's mutable data. Whenever the state of a component changes, React will re-render the component with the new data.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,14 +4,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Ryan'
+    }
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Yow Yow {this.state.name}! Wah a gwaan man!
           </p>
+          <button>Change Name</button>
           <a
             className="App-link"
             href="https://reactjs.org"
