7d36e97f605357920f4d3cffc6a6a608ea64dea1
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-12 12:15:00 -0400

    create monsters object and render to H1

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,24 +7,26 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-     monster1: {
-      name: 'Linda'
+      monsters:[  {
+      name: 'Linda',
      },
-     monster2:{
-      name: 'Frank'
+     {
+      name: 'Frank',
      },
-     monster3:{
-      name: 'Jacky'
-     }
+     {
+      name: 'Jacky',
+     },]
+    
     };
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.monster1.name}</h1>
-        <h1>{this.state.monster2.name}</h1>
-        <h1>{this.state.monster3.name}</h1>
+       {this.state.monsters.map((monster)=>{
+        return<h1>{monster.name}</h1>
+       })
+       }
       </div>
     );
   }
