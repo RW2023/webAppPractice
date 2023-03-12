6bfa4f607465c7e01bde24f02d290fcb17dcbf3e
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-12 19:02:47 -0400

    create empty monsters array

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,25 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: "aBcDe",
-        },
-        {
-          name: "Frank",
-          id: "fGhIj",
-        },
-        {
-          name: "Jacky",
-          id: "pQrSt",
-        },
-
-        { name: "Andrei",
-          id: "p85St",
-      },
-        
-      ],
+      monsters: [],
     };
   }
 
