d11ca6c715b0a7140016f77ea96817ac8bd2e8b7
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-13 03:09:33 -0400

    add searchbox and console.log even to see

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -28,6 +28,9 @@ class App extends Component {
     console.log('render');
     return (
       <div className="App">
+        <input className="search-box" type= "search" placeholder="search monsters" onChange={(event)=>{
+          console.log(event);
+        }}/>
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
