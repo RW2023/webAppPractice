f4b76fdd64bf4f3c2f4450bffb1014f8de7811b9
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-21 06:22:49 -0400

    This.state now returns  empty searchString searchString is now available throughout component move filteredMonsters out of state and render so its available throughout the component filter now works in both directions as the original array is what is being searched when you delete letters now and not filteredMonsters which kept getting smaller each time through.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [ ]
+      monsters: [],
+      searchString: ""
     };
     console.log('constructor');
   }
@@ -15,30 +16,32 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(()=>{
-        return { monsters: users };  
+        this.setState(() => {
+          return { monsters: users };
         },
-        ()=>{
-          console.log(this.state);
-        }
-      ));
+          () => {
+            console.log(this.state);
+          }
+        ));
   }
 
   render() {
     console.log('render');
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchString);
+    });
+
     return (
       <div className="App">
-        <input className="search-box" type= "search" placeholder="search monsters" onChange={(event)=>{
+        <input className="search-box" type="search" placeholder="search monsters" onChange={(event) => {
           console.log(event.target.value);
           const searchString = event.target.value.toLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster)=>{
-return monster.name.toLowerCase().includes(searchString);
+
+          this.setState(() => {
+            return { searchString: searchString };
           });
-          this.setState(()=>{
-            return{monsters: filteredMonsters}
-          })
-        }}/>
-        {this.state.monsters.map((monster) => {
+        }} />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
